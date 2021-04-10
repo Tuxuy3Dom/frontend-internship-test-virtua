15816066b2e5d182c5fc51e30caa78135aff4444
Tuxuy3Dom <ihor.yaremchuk2120@gmail.com>
2021-04-10 17:52:33 +0200

    Task 2

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,24 @@
 const clickMeButton = document.querySelector('#show-popup-form');
 const modalPopup = document.querySelector('.popup');
 const closeModalButton = document.querySelector('.btn-close');
+const formInput = document.querySelector('.form-input');
+const submitButton = document.querySelector('.submit');
 
 const openLoginModal = () => {modalPopup.classList.add('show')};
 const closeLoginModal = () => {modalPopup.classList.remove('show')};
 
+clickMeButton.addEventListener('click', () => {if (clickMeButton.innerHTML === 'Click me!')  openLoginModal();});
+closeModalButton.addEventListener('click', closeLoginModal);
 
-clickMeButton.addEventListener('click', () => {
-   if (clickMeButton.innerHTML === 'Click me!')  openLoginModal();
+formInput.addEventListener('focus', () => {
+    if (formInput.valid) {
+        // Validaton - value correct
+    } else {
+        // Validaton - value incorrect
+    }
 });
 
-closeModalButton.addEventListener('click', closeLoginModal);
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    // Validation
+});
