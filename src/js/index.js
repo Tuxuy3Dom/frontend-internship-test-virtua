10dc2d532dd39f2d50477b03f7f767b8ca9c83f6
Tuxuy3Dom <ihor.yaremchuk2120@gmail.com>
2021-04-10 17:18:43 +0200

    Task 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
 /* Here goes your JS code */
+const clickMeButton = document.querySelector('#show-popup-form');
+const modalPopup = document.querySelector('.popup');
+const closeModalButton = document.querySelector('.btn-close');
+
+const openLoginModal = () => {modalPopup.classList.add('show')};
+const closeLoginModal = () => {modalPopup.classList.remove('show')};
+
+
+clickMeButton.addEventListener('click', () => {
+   if (clickMeButton.innerHTML === 'Click me!')  openLoginModal();
+});
+
+closeModalButton.addEventListener('click', closeLoginModal);
